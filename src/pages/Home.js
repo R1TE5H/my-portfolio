56335b05793166b059fbb78d33be654ecd2c00ff
Ritesh <riteshpersaud2003@gmail.com>
2024-02-25 18:36:16 -0500

    Experiences

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -49,7 +49,7 @@ export default function Home() {
         </div>
       </div>
       <div className="section">
-        <p className="subHead">Skills</p>
+        <p className="hero">Skills</p>
         <div className="card-container">
           {skills.map((skill) => (
             <div className="skills-card center columns">
@@ -61,6 +61,75 @@ export default function Home() {
               <h3>{skill.skill}</h3>
             </div>
           ))}
+        </div>
+      </div>
+      <div
+        id="section"
+        style={{
+          backgroundColor: `var(--smoke)`,
+          padding: "2% 15% 5% 15%",
+          marginBottom: "2%",
+        }}
+      >
+        <p className="hero center">Experiences</p>
+        <div>
+          <p className="subHero">Researcher and Developer</p>
+          <p style={{ marginBottom: "15px" }}>
+            LaunchPad@Stevens | 2022 - Present
+          </p>
+          <ul type="circle">
+            <li>
+              Research Web.3 and Blockchain Technologies as well as Artificial
+              Intelligence and Machine Learning Developments
+            </li>
+            <li>Create Application Prototypes using Figma Prototype Design</li>
+            <li>
+              Create engaging websites and applications using HTML, CSS,
+              JavaScript, and React.JS
+            </li>
+            <li>
+              Develop algorithms to accomplish desired tasks using JavaScript,
+              Python, C++, and Java
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p className="subHero">
+            Lead Algorithm Developer and Circuit Builder
+          </p>
+          <p style={{ marginBottom: "15px" }}>
+            Gallois Autonomous Robot Competition | Spring 2023
+          </p>
+          <ul type="circle">
+            <li>
+              Tasked with designing and developing an autonomous robot capable
+              of navigating an obstacle course and avoiding collisions.
+            </li>
+            <li>
+              Created the Robot using SolidWorks CAD and FlashPrint Slicing to
+              design and print the body which was used to house a WeMos Circuit
+              Board, Motor, and Ultrasonic Sensors.
+            </li>
+            <li>
+              Created Path-Finding, Target Recognition and Obstacle Avoidance
+              algorithms using C++ and the Arduino IDE.
+            </li>
+            <li>Placed 6th out of 150 teams</li>
+          </ul>
+        </div>
+        <div>
+          <p className="subHero">Intern</p>
+          <p style={{ marginBottom: "15px" }}>WellCast ROI | Summer 2021</p>
+          <ul type="circle">
+            <li>
+              Reviewed and ran the WellCast algorithm with various data inputs
+            </li>
+            <li>
+              Developed client presentation within a team environment based on
+              the WellCast algorithm's results
+            </li>
+            <li>Collaborated in teams of 3-4</li>
+          </ul>
         </div>
       </div>
       <div className="section">
